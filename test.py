500e4d055f981d9436b85a43b50e3f925a89a5af
Will Koster <jameswk2@illinois.edu>
2016-08-25 09:49:35 -0500

    Write some tests

--- test.py
+++ test.py
@@ -1,28 +1,39 @@
+import unittest
 from hw0 import *
 
-for i, j in {missing_friends('james', 
-                         '''
-                         james jack jill
-                         jack jill james
-                         jill jack james
-                         '''): 0, 
+class TestHW0(unittest.TestCase):
+    def test_missing_friends(self):
+        for i, j in {missing_friends('james', 
+                                 '''
+                                 james jack jill
+                                 jack jill james
+                                 jill jack james
+                                 '''): 0, 
+        
+                     missing_friends('james', 
+                                 '''
+                                 james jack
+                                 jack james jill bob
+                                 jill jack bob
+                                 bob jill jack
+                                 '''): 2,
+        
+                     missing_friends('james', 
+                                 '''
+                                 james jack
+                                 jack james jill
+                                 jill jack
+                                 '''): 1
+                     }.iteritems():
+            self.assertEqual(i, j)
+        def test_reverse_string(self):
+            # self.assertEqual(reverse_string("apples"), "selppa")
+            # self.assertEqual(reverse_string("apple"), "elppa")
+            # self.assertEqual(reverse_string("Hello World", "dlroW olleH"))
 
-             missing_friends('james', 
-                         '''
-                         james jack
-                         jack james jill bob
-                         jill jack bob
-                         bob jill jack
-                         '''): 2,
+        def test_oddEven(self):
+            # self.assertEqual("odd", oddEven("one"))
+            # self.assertEqual("even", oddEven("four"))
 
-             missing_friends('james', 
-                         '''
-                         james jack
-                         jack james jill
-                         jill jack
-                         '''): 1
-             }.iteritems():
-    if  i == j:
-        print "Everything is good"
-    else:
-        print "Something bad happened; returned %d when I wanted %d" % (i, j)
+        def test_sign_flipper(self):
+            pass
