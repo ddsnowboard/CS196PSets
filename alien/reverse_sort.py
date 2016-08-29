1f6662c2e8b06da2cba668d26c52180fa1866ab1
Will Koster <jameswk2@illinois.edu>
2016-08-29 10:13:56 -0500

    Make alien work
    I think, at least. I'm not 100% sure, but the unit test they gave me
    passed, so I think it's good.

--- alien/reverse_sort.py
+++ alien/reverse_sort.py
@@ -1,3 +1,16 @@
 from itertools import cycle
+from random import shuffle
 def reverse_sort(strings):
-    letters = {a: None for a in reduce(lambda a, b: a + b, strings)}
+    letters = list(set([a for a in reduce(lambda a, b: a + b, strings)]))
+    stopped = False
+    while True:
+        stopped = False
+        for s in strings:
+            for c1, c2 in zip(s, s[1::]):
+                if letters.index(c2) < letters.index(c1):
+                    stopped = True
+                    letters.pop(letters.index(c2))
+                    letters.insert(letters.index(c1) + 1, c2)
+        if not stopped:
+            break
+    return letters
--- alien/test.py
+++ alien/test.py
@@ -1,4 +1,8 @@
 import unittest
+from reverse_sort import *
 class TestSortReverse(unittest.TestCase):
     def test_reverse_string(self):
-        self.assertEqual(["ace", "moz", "acdem"], ["a", "c", "d", "e", "m", "o", "z"]) 
+        self.assertEqual(reverse_sort(["ace", "moz", "acdem"]), ["a", "c", "d", "e", "m", "o", "z"]) 
+
+if __name__ == "__main__":
+    unittest.main()
